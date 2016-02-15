efba0ac68c83f261c2dab37651c4fb9351fdfdaa
Vijayandra Singh <vsingh.usa@gmail.com>
2016-02-14 23:57:56 -0500

    Sun Feb 14 23:57:56 EST 2016

--- Oscilloscope/windows_intel/x32/timechg_signal.c
+++ Oscilloscope/windows_intel/x32/timechg_signal.c
@@ -1,7 +1,7 @@
 // Please note :
 // this is script "C"
 // Do not compile this code, it should run as it is
-// uses:
+// uses: Run this code which GUI is up and connected to HW ( Green LED is On)
 // $$ Prompt >> run_myscript timechg_signal.c
 #include <stdio.h>
 
@@ -27,6 +27,18 @@ int main(void)
 
     while(1)
     {
+        // following code produces signal which increasing amplitude
+        // and other decreasing amplitude
+        // Cl_signal
+        // Arg 1 : TMR
+        // Arg 2 : TMRDIV
+        // Arg 3 : Number of Element
+        // Arg 4 : Sig1 Type [ 0 SIN,1 COS, 2 SQUARE, 3 Rectgle, 4 Triangular,5 Trgn Sym1,6 Trng Sym2
+        // Arg 5 : Sig1 Min
+        // Arg 6 : Sig1 Max
+        // Arg 7 : Sig2 Type [ 0 SIN,1 COS, 2 SQUARE, 3 Rectgle, 4 Triangular,5 Trgn Sym1,6 Trng Sym2
+        // Arg 8 : Sig2 Min
+        // Arg 9 : Sig2 Max
         for(i=0;i<5000;i+=10)
         {
             Cl_signal(tmr,tmrdiv,numElem,sig1type,sig1min,i,sig2type,sig2min,5000-i);
