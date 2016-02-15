8840506fa29bc7da8445fc89f3dc06f13bec7cee
Vijayandra Singh <vsingh.usa@gmail.com>
2016-02-14 19:28:21 -0500

    Sun Feb 14 19:28:21 EST 2016

---
+++ Oscilloscope/ubuntu_intel_64/timechg_signal.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main(void)
+{
+    char a[20];
+    int x=0;
+
+    int tmr=500;
+    int tmrdiv=0;
+    int numElem=400;
+
+    int sig1type=0;
+    int sig1min=0;
+    int sig1max=4000;
+
+    int sig2type=0;
+    int sig2min=0;
+    int sig2max=4000;
+    int i;
+
+    printf("Change tab continous press Ctrl-C to exit\n");
+
+    while(1)
+    {
+        for(i=0;i<5000;i+=10)
+        {
+            Cl_signal(tmr,tmrdiv,numElem,sig1type,sig1min,i,sig2type,sig2min,5000-i);
+            ClWait(100);
+        }
+    }
+    return 0;
+}
